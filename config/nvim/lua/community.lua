beea491fb0369d120444d44a20301f1186a7ba88
Jesse Darkovski <j.darkovski@gmail.com>
2026-03-13 23:03:11 +1100

    add pack.rust to community

--- config/nvim/lua/community.lua
+++ config/nvim/lua/community.lua
@@ -7,5 +7,6 @@ return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.utility.live-server-nvim" },
+  { import = "astrocommunity.pack.rust" },
   -- import/override with your plugins folder
 }
