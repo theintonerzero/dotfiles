4fbc2de294026618ff96abc1779089862830198d
Jesse Darkovski <j.darkovski@gmail.com>
2026-05-01 05:10:04 +1000

    feat: nvim config

--- nvim/.config/nvim/lua/plugins/astroui.lua
+++ nvim/.config/nvim/lua/plugins/astroui.lua
@@ -1,29 +1,34 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
--- AstroUI provides the basis for configuring the AstroNvim User Interface
--- Configuration documentation can be found with `:h astroui`
--- NOTE: We highly recommend setting up the Lua Language Server (`:LspInstall lua_ls`)
---       as this provides autocomplete and documentation while editing
-
 ---@type LazySpec
 return {
   "AstroNvim/astroui",
   ---@type AstroUIOpts
+  dependencies = {
+    {
+      "ellisonleao/gruvbox.nvim",
+      priority = 1000,
+      config = function()
+        require("gruvbox").setup {
+          transparent_mode = true,
+          contrast = "", -- "hard", "soft" or ""
+          italic = {
+            strings = true,
+            emphasis = true,
+            comments = true,
+            operators = false,
+            folds = true,
+          },
+        }
+      end,
+    },
+  },
   opts = {
     -- change colorscheme
-    colorscheme = "astrodark",
-    -- AstroUI allows you to easily modify highlight groups easily for any and all colorschemes
+    colorscheme = "gruvbox",
     highlights = {
-      init = { -- this table overrides highlights in all themes
-        -- Normal = { bg = "#000000" },
-      },
-      astrodark = { -- a table of overrides/changes when applying the astrotheme theme
-        -- Normal = { bg = "#000000" },
-      },
+      init = {},
+      astrodark = {},
     },
-    -- Icons can be configured throughout the interface
     icons = {
-      -- configure the loading of the lsp in the status line
       LSPLoading1 = "⠋",
       LSPLoading2 = "⠙",
       LSPLoading3 = "⠹",
--- nvim/.config/nvim/lua/plugins/mason.lua
+++ nvim/.config/nvim/lua/plugins/mason.lua
@@ -1,27 +1,46 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
--- Customize Mason
-
 ---@type LazySpec
 return {
-  -- use mason-tool-installer for automatically installing Mason packages
   {
     "WhoIsSethDaniel/mason-tool-installer.nvim",
-    -- overrides `require("mason-tool-installer").setup(...)`
     opts = {
-      -- Make sure to use the names found in `:Mason`
       ensure_installed = {
-        -- install language servers
+        -- lua
         "lua-language-server",
-
-        -- install formatters
         "stylua",
 
-        -- install debuggers
-        "debugpy",
-
-        -- install any other package
+        -- web / typescript
+        "vtsls",
+        "prettier",
+        "eslint_d",
+        "js-debug-adapter",
         "tree-sitter-cli",
+
+        -- python
+        "basedpyright",
+        "ruff",
+
+        -- go
+        "gopls",
+        "gofumpt",
+        "delve", -- go debugger
+
+        -- rust
+        "rust-analyzer",
+
+        -- dotnet
+        "csharp-language-server",
+        "netcoredbg", -- dotnet debugger
+
+        -- shell / bash
+        "bash-language-server",
+        "shfmt",
+        "shellcheck",
+
+        -- misc
+        "json-lsp",
+        "yaml-language-server",
+        "taplo", -- toml
+        "marksman", -- markdown
       },
     },
   },
